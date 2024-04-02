f0d2b3c7eddf4bd2e1bf586abcf8265660a73c79
LayconJohn <laycon.gomes@tld.com.br>
2024-04-02 17:54:16 -0300

    feat(user): create user endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,17 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { createUser } from "./user.service";
+import { CreateUserSchema } from "./user.schema";
 
-export async function registerUserHandler(request: FastifyRequest, reply: FastifyReply) {
+export async function registerUserHandler(request: FastifyRequest<{
+   Body: CreateUserSchema     
+}>, reply: FastifyReply) {
+    const body = request.body;
 
+    try {
+        const user = await createUser(body);
+        return reply.code(201).send(user)
+    } catch(err) {
+        console.log(err);
+        return reply.code(500).send(err)
+    }
 }
