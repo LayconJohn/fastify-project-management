330f84a5eccd26bc6d8a77135640fc0262a787a8
LayconJohn <laycon.gomes@tld.com.br>
2024-04-03 11:58:53 -0300

    feat(user): update user schema and implements createUserResponseSchema

--- src/modules/user/user.schema.ts
+++ src/modules/user/user.schema.ts
@@ -1,15 +1,30 @@
-import { z } from "zod"
+import { z } from "zod";
+import { buildJsonSchemas } from "fastify-zod"
 
-const createUserSchema = z.object({
+const userCore = {
     email: z.string({
         required_error: "Email is Required",
         invalid_type_error: "email must be a string"
     }).email(),
     name: z.string(),
+}
+
+const createUserSchema = z.object({
+    ...userCore,
     password: z.string({
         required_error: "Password is Required",
         invalid_type_error: "Password must be a string"
     })
 })
 
+const createUserResponseSchema = z.object({
+    id: z.number(),
+    ...userCore
+})
+
 export type CreateUserSchema = z.infer<typeof createUserSchema>
+
+export const { schemas: userSchemas, $ref } = buildJsonSchemas({
+    createUserSchema,
+    createUserResponseSchema
+})
